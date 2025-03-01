88f20e0e636c45f1c3efe33c48e9cff387b909e8
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-01 12:47:48 +0330

    feat :style added to navBar component

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,13 +1,13 @@
 
 export default function Navbar() {
   return (
-    <div>
+    <div className="p-2 bg-blue-800"> 
 
-    <ul className="flex  ">
-        <li>Home</li>
-        <li>Products</li>
-        <li>Cart</li>
-        <li>Add Product</li>
+    <ul className="flex child:p-2 child:bg-blue-800 child:text-white child:cursor-pointer child:transition-all child:rounded-lg ">
+        <li className="hover:bg-blue-500">Home</li>
+        <li className="hover:bg-blue-500">Products</li>
+        <li className="hover:bg-blue-500">Cart</li>
+        <li className="hover:bg-blue-500">Add Product</li>
     </ul>
 
     </div>
