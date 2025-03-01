da96576c0378e47f1ecf6b1598d6dcdb46887cbd
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-01 14:57:32 +0330

    feat : active link added

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,16 +1,53 @@
-import React from 'react';
+import React from "react";
+import {  NavLink } from "react-router";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="p-2 bg-blue-800">
-      <ul className="flex child:p-2 child:bg-blue-800 child:text-white child:cursor-pointer child:transition-all child:rounded-lg">
-        <li className="hover:bg-blue-500">Home</li>
-        <li className="hover:bg-blue-500">Products</li>
-        <li className="hover:bg-blue-500">Cart</li>
-        <li className="hover:bg-blue-500">Add Product</li>
-      </ul>
+    <div className="flex gap-4 bg-blue-900 p-2">
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive
+            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500   transition-all "
+            : "bg-blue-900 text-white rounded-lg  p-2  hover:bg-blue-500 transition-all "
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/products"
+        className={({ isActive }) =>
+          isActive
+            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500   transition-all "
+            : "bg-blue-900 text-white rounded-lg  p-2  hover:bg-blue-500 transition-all "
+        }
+      >
+        Products
+      </NavLink>
+      <NavLink
+        to="/cart"
+        className={({ isActive }) =>
+          isActive
+            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500   transition-all "
+            : "bg-blue-900 text-white rounded-lg  p-2  hover:bg-blue-500 transition-all "
+        }
+      >
+        Cart
+       
+      </NavLink>
+      <NavLink
+        to="/addproduct"
+        className={({ isActive }) =>
+          isActive
+            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500   transition-all "
+            : "bg-blue-900 text-white rounded-lg  p-2  hover:bg-blue-500 transition-all "
+        }
+      >
+        Add Product
+      </NavLink>
     </div>
+      
   );
-}
+};
 
 export default Navbar;
