45adba9e4fef5cf15a9aa11580eeb4c0b1285172
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-01 11:01:46 +0330

    feat : json-server data base added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <h1 className='bg-red-200'>hello world</h1>
+     <h1 className='bg-red-400'>hello world</h1>
     </>
   )
 }
