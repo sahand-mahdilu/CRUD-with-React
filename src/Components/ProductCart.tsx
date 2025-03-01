85f665cf669536e5b625606376d3b0148222f2d5
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-01 15:38:23 +0330

    feat : cart content added

--- src/Components/ProductCart.tsx
+++ src/Components/ProductCart.tsx
@@ -1,6 +1,20 @@
 
 export default function ProductCart() {
   return (
-    <div>ProductCart</div>
+    <div className="flex flex-col items-center gap-5 bg-slate-600 p-3 rounded-2xl">
+
+        <h1 className="text-3xl font-bold">Shampoo</h1>
+        <p className="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, adipisci?</p>
+        <div>
+        <span className="text-2xl font-bold text-orange-500">Price: </span>
+        <span className="text-2xl">800$ </span>
+
+
+        </div>
+        <span>count :20</span>
+        <button className="p-1 px-2 rounded-lg bg-green-400">Add to cart</button>
+
+
+    </div>
   )
 }
--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -1,6 +1,18 @@
+import ProductCart from "../Components/ProductCart";
 
 export default function Products() {
   return (
-    <div className="text-white">Products</div>
-  )
+    <div className="text-white">
+      <span>products</span>
+
+      <div className="grid grid-cols-3 gap-5 p-5"> 
+
+      <ProductCart/>
+      <ProductCart/>
+      <ProductCart/>
+      </div>
+
+
+    </div>
+  );
 }
