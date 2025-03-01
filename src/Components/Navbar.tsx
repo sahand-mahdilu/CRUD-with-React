4b8635b67058e482ed9dff704a97f4537e791677
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-01 14:59:46 +0330

    feat : type added

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,52 +1,50 @@
 import React from "react";
-import {  NavLink } from "react-router";
+import { NavLink } from "react-router";
 
 const Navbar: React.FC = () => {
   return (
     <div className="flex gap-4 bg-blue-900 p-2">
       <NavLink
         to="/"
-        className={({ isActive }) =>
+        className={({ isActive }: { isActive: boolean }) =>
           isActive
-            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500   transition-all "
-            : "bg-blue-900 text-white rounded-lg  p-2  hover:bg-blue-500 transition-all "
+            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500 transition-all"
+            : "bg-blue-900 text-white rounded-lg p-2 hover:bg-blue-500 transition-all"
         }
       >
         Home
       </NavLink>
       <NavLink
         to="/products"
-        className={({ isActive }) =>
+        className={({ isActive }: { isActive: boolean }) =>
           isActive
-            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500   transition-all "
-            : "bg-blue-900 text-white rounded-lg  p-2  hover:bg-blue-500 transition-all "
+            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500 transition-all"
+            : "bg-blue-900 text-white rounded-lg p-2 hover:bg-blue-500 transition-all"
         }
       >
         Products
       </NavLink>
       <NavLink
         to="/cart"
-        className={({ isActive }) =>
+        className={({ isActive }: { isActive: boolean }) =>
           isActive
-            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500   transition-all "
-            : "bg-blue-900 text-white rounded-lg  p-2  hover:bg-blue-500 transition-all "
+            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500 transition-all"
+            : "bg-blue-900 text-white rounded-lg p-2 hover:bg-blue-500 transition-all"
         }
       >
         Cart
-       
       </NavLink>
       <NavLink
         to="/addproduct"
-        className={({ isActive }) =>
+        className={({ isActive }: { isActive: boolean }) =>
           isActive
-            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500   transition-all "
-            : "bg-blue-900 text-white rounded-lg  p-2  hover:bg-blue-500 transition-all "
+            ? "text-white rounded-lg p-2 hover:bg-blue-500 bg-blue-500 transition-all"
+            : "bg-blue-900 text-white rounded-lg p-2 hover:bg-blue-500 transition-all"
         }
       >
         Add Product
       </NavLink>
     </div>
-      
   );
 };
 
