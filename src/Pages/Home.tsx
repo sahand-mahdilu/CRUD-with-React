efc398920fc9968d55de58a9028463e99b776a09
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-01 12:58:04 +0330

    feat : home page content added

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,6 +1,10 @@
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="text-white flex flex-col items-center mt-16">
+        <h1 className="text-4xl animate-pulse">Welcome to Galexy Shop</h1>
+        <p className="w-[70%] text-center mt-10">Discover the secret to radiant, glowing skin with our exclusive range of beauty and personal care products. From luxurious shampoos that nourish your hair to invigorating body lotions that leave your skin feeling soft and rejuvenated, we have everything you need to pamper yourself from head to toe. Our carefully curated selection includes premium-quality ingredients that are gentle on your skin and deliver exceptional results. Treat yourself to the best in beauty and self-care with our trusted products and experience the difference for yourself!</p>
+
+    </div>
   )
 }
