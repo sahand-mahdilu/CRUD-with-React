a053d168eb3eaa6fca25670273fd40d894548c98
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-02 08:27:15 +0330

    feat : api created

---
+++ src/API/APIs.ts
@@ -0,0 +1,7 @@
+
+const baseURL : string="http://localhost:3000"
+
+
+
+
+export {baseURL}
--- src/Components/ProductCart.tsx
+++ src/Components/ProductCart.tsx
@@ -13,6 +13,16 @@ export default function ProductCart() {
         </div>
         <span>count :20</span>
         <button className="p-1 px-2 rounded-lg bg-green-400">Add to cart</button>
+        <span>    <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            fill="currentColor"
+            viewBox="0 0 16 16"
+          >
+            <path d="M16 8s-3-5-8-5-8 5-8 5 3 5 8 5 8-5 8-5zm-8 4a4 4 0 1 1 0-8 4 4 0 0 1 0 8z" />
+            <path d="M8 5a3 3 0 1 0 0 6 3 3 0 0 0 0-6z" />
+          </svg></span>
 
 
     </div>
