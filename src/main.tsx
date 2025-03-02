f2529caf0631b1deb083ea41902401aabdef0903
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-02 09:20:50 +0330

    feat: query setting added

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { client } from './Lib/ReactQuerySeting.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={client}>
     <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
