5750ff2cccee17d85427901d975b0d4dd49543c0
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-03 10:50:56 +0330

    feat : deleting data from cart completed

--- src/Pages/Cart.tsx
+++ src/Pages/Cart.tsx
@@ -1,8 +1,11 @@
 
 
 import ProductCart from "../Components/ProductCart";
+import { endPoints } from "../Constant/URLs";
+import useDeleteProducts from "../Hooks/useDeleteProducts";
 
 import useGetCartProducts from "../Hooks/useGetCartProducts"
+import { DeleteProductArgs } from "../Models/ProductModels";
 
 export default function Cart() {
 
@@ -12,11 +15,13 @@ export default function Cart() {
 
     console.log(data);
 
-    // const {mutate}=useDeleteProducts()
+    const {mutate}=useDeleteProducts()
 
 
-
-
+    const deleteProductHandler = (productId: string, endPoint: string) => {
+      const args: DeleteProductArgs = { id: productId, endpoint: endPoint };
+      mutate(args);
+    };
 
   return (
     <>    <div className="text-white text-2xl font-bold text-center mt-4 animate-pulse ">Cart</div>
@@ -24,7 +29,7 @@ export default function Cart() {
 <div className="grid grid-cols-3 gap-5 p-16 bg-black">
 
   {data?.map(d=>{return <ProductCart key={d.id} {...d} >
-    <button onClick={}  className="bg-red-500 text-white p-1 px-3 rounded-lg hover:bg-red-400">Delete</button>
+    <button onClick={()=>deleteProductHandler(String(d.id),endPoints.cart)}  className="bg-red-500 text-white p-1 px-3 rounded-lg hover:bg-red-400">Delete</button>
   </ProductCart>})}
 
     
