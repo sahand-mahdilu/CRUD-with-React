1825ebe6402b3af965cafb2af1aa186b7c5fe9f2
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-03 18:40:40 +0330

    feat : form content added

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,9 +1,45 @@
+import { useForm } from "react-hook-form";
 
 export default function Form() {
+  const { register, handleSubmit } = useForm({
 
+    defaultValues:{
+        name: "",
+        description: "",
+        qyt: 0,
+        price: 0,
+        available: false,
+    }
+  });
 
-    
   return (
-    <div>Form</div>
+    <form className="flex flex-col bg-slate-800 p-10 rounded-2xl " action="">
+        <label className="text-white pb-1" htmlFor="nameInput">Name : </label>
+        <input className="p-2 rounded-md" id="nameInput" placeholder="Enter name " type="text" />
+
+         <label className="mt-5 pb-1" htmlFor="DesInput">Description : </label>
+         <textarea className="rounded-md" id="DesInput"  />   
+
+         <label className="mt-5 pb-1" htmlFor="quantity">qty : </label>
+         <input id="quntity" className="p-2 rounded-md " placeholder="Enter quantity" type="number" />
+
+         <label className="mt-5 pb-1" htmlFor="priceInput">Price : </label>
+         <input id="priceInput" className="p-2 rounded-md " placeholder="Enter Price" type="number" />
+        
+
+         <div className="mt-5 " >
+         <label className="mt-5 pb-1" htmlFor="avail">Availabe : </label>
+         <input  id="avail" className="size-4 rounded-md " type="checkbox" />
+         </div>
+
+
+         <button className="bg-blue-600 mt-3 p-2 rounded-md hover:bg-blue-400">Add Porduct</button>
+
+
+
+
+
+
+    </form>
   )
 }
--- src/Pages/AddProduct.tsx
+++ src/Pages/AddProduct.tsx
@@ -1,12 +1,16 @@
+import Form from "../Components/Form";
 
 export default function AddProduct() {
   return (
     <div className="text-white">
 
       <h1 className="text-2xl font-bold  mt-5 animate-pulse text-center">Add product</h1>
+      <div className="flex justify-center items-center mt-8">
+        <Form/>
+
+      </div>
 
 
-      
 
     </div>
   )
