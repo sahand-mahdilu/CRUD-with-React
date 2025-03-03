030a6142e018a858a63f8049aaa4e32173912f14
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-03-03 11:02:22 +0330

    feat : queryClient for real time update added

--- src/Hooks/useDeleteProducts.ts
+++ src/Hooks/useDeleteProducts.ts
@@ -1,12 +1,22 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteProduct } from "../API/APIs";
 import { DeleteProductArgs, ProductsModel } from "../Models/ProductModels";
 
 export default function useDeleteProducts() {
+
+    const queryClient = useQueryClient()
+
   return (
    useMutation <ProductsModel | undefined, Error, DeleteProductArgs>({
 
-    mutationFn:({id,endpoint})=>deleteProduct(id,endpoint)
+    mutationFn:({id,endpoint})=>deleteProduct(id,endpoint),
+
+    onSuccess:()=>{
+        queryClient.invalidateQueries({
+
+            queryKey: ["cartProducts"]
+        })
+    }
 
 
    })
